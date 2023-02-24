8a30451f3dcd111d52f6aefa58064137d0fa6d74
JMc-GH <john.mcewan@protonmail.com>
2023-02-24 22:34:00 +0000

    Quadratic Probing working - double hashing to fix

--- SortSearch/src/Hashing.java
+++ SortSearch/src/Hashing.java
@@ -1,6 +1,10 @@
 import java.util.HashMap;
 import java.util.Map;
 
+
+
+// TODO: Java code standards: 
+
 // TODO: MurmurHash  -- simple / elegant
 // TODO: SipHash-2-4  -- python
 // TODO: Fowler–Noll–Vo (FNV) hash function. -- old pythoin
@@ -12,76 +16,83 @@ import java.util.Map;
 public class Hashing {
 
 	
-	private static final int PROBE_LINEAR = 1;
-	private static final int PROBE_QUADRATIC = 2;
-	private static final int DOUBLE_HASH = 3;
+	private Integer[] hashTable;
+	public static final int PROBE_LINEAR = 1;
+	public static final int PROBE_QUADRATIC = 2;
+	public static final int DOUBLE_HASH = 3;
+	public static final int HASH_MODULUS = 1;
 	
+	private int addressingMode = PROBE_LINEAR;
+	private int hashMode = HASH_MODULUS;
+	
+	
+	// GETTERS AND SETTERS
+	
+	public int getHashMode() {
+		return hashMode;
+	}
 
-	public static int modHash(int k, int m ) {
-		return k % m;
+	public void setHashMode(int hashMode) {
+		this.hashMode = hashMode;
 	}
+
 	
-	
-	public int multiHash(double key,int slots) {
-		return (int) key * slots;
+	public Integer[] getHashTable() {
+		return hashTable;
 	}
+
+
+	public int getAddressingMode() {
+		return addressingMode;
+	}
+
+
+	public void setAddressingMode(int addressingMode) {
+		this.addressingMode = addressingMode;
+	}
+
+
 	
-	
-	public static long djb2Hash(String str) {
-	    // TODO: generated code - understand line-byline
-		// REVIEW: before exam
+	// main hashing function
+	private int hash(int k, int m ) {
 		
+		int hashValue = 1;
 		
-		long hash = 5381;
-	    for (int i = 0; i < str.length(); i++) {
-	        char c = str.charAt(i);
-	        hash = ((hash << 5) + hash) + c; // hash * 33 + c
-	    }
-	    return hash;
+		switch (hashMode) {
+		case HASH_MODULUS:
+			hashValue = k % m;
+			break;
+		default: 
+			hashValue = k % m;
+		}
+		
+		if (hashValue == 0) {
+			hashValue = 1;
+		}
+		
+		return hashValue;
 	}
 	
 	
-	
-	
-	private void openAddressingDemo(int addressingMethod,Integer[] keysToAdd,int hashMapSize) {
+	public void buildHashmap(Integer[] keysToAdd,int hashMapSize) {
 		// Collision resolution test for W6.1
+		// this populates the hashmap from an array of integers
 		
-		Integer[] hashTable = new Integer[hashMapSize+1]; /// add one to account for the base 0 array
+		hashTable = new Integer[hashMapSize+1]; /// add one to account for the base 0 array
 		
 		int m = hashMapSize; 
 		
 		System.out.println("Initial hash function results");
 		System.out.println("-----------------------------");
 		System.out.println("key \t : \t hash");
+		
 		for (int k : keysToAdd) {
-			int i = modHash(k,m);
+			int i = hash(k,m);
 			System.out.println(k + "\t : \t" + i);
 			}
 		
-		System.out.println();
-		System.out.println("Using Method: " + addressingMethod);
-		System.out.println("-----------------------------");
-		
-		
 		for (int k : keysToAdd) {
-				int i = modHash(k,m);
-				System.out.println("Try key " + k + " in " + i);
-				while (hashTable[i] != null) {			//loop until a free slot is found
-					System.out.println("COLLISION (contains "+hashTable[i]+ ") try \t" + (i+1));
-					switch (addressingMethod) {
-					case 1: // linear probing
-						i++;
-						break;
-					case 2: // Quadratic addressing
-						i++;
-						i = i^2;
-					default: // Double Hashing
-						i = modHash(i,m);
-					}
-					
-				}
-			System.out.println("Added Slot " + i);
-				hashTable[i] = k;
+				insert(k,m);
 			}
 		
 		System.out.println();
@@ -95,24 +106,54 @@ public class Hashing {
 		
 	}
 	
+	private int quadHash(int key,int counter) {
+		
+		int ret = key + counter^2;
 	
-	public static void main(String[] args) {
+		if (ret==0)ret = 1;
+		
+		return ret;
 
-		Hashing testHash = new Hashing() ;
+	}
+	public void insert(int k,int m) {
 		
-//		Integer[] linearTest = {5, 28, 19, 15, 20, 33, 12, 17, 10};
-//		
-//		testHash.openAddressingDemo(PROBE_LINEAR,linearTest,9);
-	
-		Integer[] quadraticTest = {10, 22, 31, 4, 15, 28, 17, 88, 59};
+		int i = hash(k,m);		//generates the hash
+		int j = 0; 
+		System.out.println("Try key " + k + " in " + i);
 		
-		testHash.openAddressingDemo(PROBE_QUADRATIC, quadraticTest, 11);
-		
-		
-		
-
-			
+		while (hashTable[i] != null) {			//loop until a free slot is found
+			switch (addressingMode) {
+			case 1: // linear probing
+				i++;
+				System.out.println("COLLISION try LINEAR \t" + (i));
+				break;
+			case 2: // Quadratic addressing
+				j++;
+				i = quadHash(i,j);
+				System.out.println("COLLISION try QUADRATIC \t" + (i));
+				break;
+			default: // Double Hashing
+				i++;
+				i = i * (1 + (k % m));
+				
+			}
 		}
+		System.out.println("Added Slot " + i);
+		hashTable[i] = k;
+		
+		
+	}
+	
+	public int search(int k) {
+		//TODO : Should count deleted items?
+		return 0;
+	}
+	
+	public void delete(int k) {
+		//TODO : Add deleted marker?
+	}
+	
+
 
 		
 		
---
+++ SortSearch/src/Test_Code.java
@@ -0,0 +1,27 @@
+
+public class Test_Code {
+
+
+
+	public static void main(String[] args) {
+
+		
+		
+		Hashing testHash = new Hashing() ;
+
+		// Linear 
+//		Integer[] linearTest = {5, 28, 19, 15, 20, 33, 12, 17, 10};
+//		Helpers.printArray(linearTest);
+//		
+//		testHash.buildHashmap(linearTest,9);
+		
+		
+	
+		
+		Integer[] quadraticTest = {10, 22, 31, 4, 15, 28, 17, 88, 59};
+	
+		testHash.setAddressingMode(Hashing.PROBE_QUADRATIC);
+		testHash.buildHashmap(quadraticTest, 11);
+	}
+
+}
